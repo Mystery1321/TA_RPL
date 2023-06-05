f4c5b88115b49aaafb6de02635f72aed8397fda7
artapages <93313578+artapages@users.noreply.github.com>
2023-06-06 01:24:03 +0700

    delete lagi

--- rental/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rental {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.rental to javafx.fxml;
-    exports com.example.rental;
-}
